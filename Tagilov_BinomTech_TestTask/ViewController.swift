b71b7934a391ca21c125558bd44596b2e2af5552
vtagilov <v.tagilov@mail.ru>
2024-03-07 20:55:22 +0300

    Initial Commit

---
+++ Tagilov_BinomTech_TestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tagilov_BinomTech_TestTask
+//
+//  Created by Владимир on 07.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
